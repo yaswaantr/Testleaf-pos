bf9deee3d730ea5b9ef49940aee77ac905850ad5
Mohan Balakrishnan <38653431+mohan-balakrishnan@users.noreply.github.com>
2023-11-06 10:21:56 +0530

    Create ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+{
+  "apps": [
+    {
+      "name": "web",
+      "script": "yarn",
+      "args": "run dev:web",
+      "cwd": "/app"
+    },
+    {
+      "name": "api",
+      "script": "yarn",
+      "args": "run dev:api",
+      "cwd": "/app/api"
+    }
+  ]
+}
